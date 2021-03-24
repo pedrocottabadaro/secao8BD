8ee8b48673b14585ac7cbe567f8fd69c6558886c
Victor Guerra <victorguerra@ice.ufjf.br>
2021-03-24 08:43:11 -0300

    Alterando model produto

--- model/produto/produto.php
+++ model/produto/produto.php
@@ -10,7 +10,7 @@ class Produto{
 
         $dados = array();
 
-        $sql = "SELECT * FROM  WHERE  = ";
+        $sql = "SELECT * FROM produto";
 
         try{
 
@@ -39,7 +39,7 @@ class Produto{
 
        
 
-        $sql = "SELECT * FROM pergunta WHERE CD_PERGUNTA = $id";
+        $sql = "SELECT * FROM produto WHERE CD_PRODUTO = $id";
 
         try{
 
@@ -64,12 +64,48 @@ class Produto{
 
 
 
-    public function insertProduto() {
+    public function insertProduto($data) {
         global $conn;
 
        
 
-        $sql = "INSERT INTO  VALUES (NULL, , , )";
+        $sql = "INSERT INTO produto VALUES (NULL, '{$data['nome']}', '{$data['unidade']}', {$data['peso']}, {$data['qtd_estoque']}, {$data['preco']})";
+
+        try{
+
+            $stmt=$conn->prepare($sql);
+            $stmt->execute();
+
+        } 
+        catch(PDOException $e){
+            echo $e->getMessage();
+        }
+    }
+
+    public function updateProduto($data) {
+        global $conn;
+
+       
+
+        $sql = "UPDATE produto SET NOME = '{$data['nome']}', UNIDADE = '{$data['unidade']}', PESO = {$data['id']}, QTD_ESTOQUE = {$data['qtd_estoque']}, PRECO = {$data['preco']} WHERE CD_PRODUTO = {$data['id']}";
+
+        try{
+
+            $stmt=$conn->prepare($sql);
+            $stmt->execute();
+
+        } 
+        catch(PDOException $e){
+            echo $e->getMessage();
+        }
+    }
+
+    public function deleteProduto($id) {
+        global $conn;
+
+       
+
+        $sql = "DELETE FROM produto WHERE CD_PRODUTO = $id";
 
         try{
 
